f66fdb5c3bebdf693f62884ffc06a40b93328bb5
Alex Young <alex@helicoid.net>
2010-11-22 19:34:24 +0000

    Added confirm() to delete client-side code

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,21 +1,23 @@
 (function() {
   $('.destroy').live('click', function(e) {
-    var element = $(this),
-        form = $('<form></form>');
     e.preventDefault();
-    form
-      .attr({
-        method: 'POST',
-        action: element.attr('href')
-      })
-      .hide()
-      .append('<input type="hidden" />')
-      .find('input')
-      .attr({
-        'name': '_method',
-        'value': 'delete'
-      })
-      .end()
-      .submit();
+    if (confirm('Are you sure you want to delete that item?')) {
+      var element = $(this),
+          form = $('<form></form>');
+      form
+        .attr({
+          method: 'POST',
+          action: element.attr('href')
+        })
+        .hide()
+        .append('<input type="hidden" />')
+        .find('input')
+        .attr({
+          'name': '_method',
+          'value': 'delete'
+        })
+        .end()
+        .submit();
+    }
   });
 })();
