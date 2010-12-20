3e010544717a8a0cc384d834f531d241ade579b7
Alex Young <alex@helicoid.net>
2010-12-20 16:51:58 +0000

    Added Aristo, jQuery UI, and started adding the ajax interface

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,19 @@
 (function() {
+  // Easily get an item's database ID based on an id attribute
+  $.fn.itemID = function() {
+    try {
+      var items = $(this).attr('id').split('-');
+      return items[items.length - 1];
+    } catch (exception) {
+      return null;
+    }
+  };
+
+  $.put = function(url, data, success) {
+    data._method = 'PUT';
+    $.post(url, data, success, 'json');
+  };
+
   $('.destroy').live('click', function(e) {
     e.preventDefault();
     if (confirm('Are you sure?')) {
@@ -20,4 +35,65 @@
         .submit();
     }
   });
+
+  // Correct widths and heights based on window size
+  function resize() {
+    var height = $(window).height() - $('#header').height() - 1,
+        width = $('.content').width(),
+        ov = $('.outline-view'),
+        ed = $('#editor'),
+        toolbar = $('.toolbar'),
+        divider = $('.content-divider'),
+        content = $('.content'),
+        controls = $('#controls');
+
+    $('#DocumentTitles').css({ height: height - toolbar.height() + 'px' });
+    ov.css({ height: height + 'px' });
+    toolbar.css({ width: ov.width() + 'px' });
+
+    content.css({
+      height: height - controls.height() + 'px',
+      width: $('body').width() - ov.width() - divider.width() - 1 + 'px'
+    });
+
+    divider.css({ height: height + 'px' });
+
+    ed.css({
+      width: content.width() - 5 + 'px',
+      height: content.height() - 5 + 'px'
+    }).focus();
+
+    $('#controls').css({
+      width: content.width() + 'px'
+    });
+  }
+
+  $('#document-list li a').live('click', function(e) {
+    var li = $(this);
+
+    $.get(this.href + '.json', function(data) {
+      $('#document-list .selected').removeClass('selected');
+      li.addClass('selected');
+      $('#editor').html(data.data);
+      $('#editor').focus();
+    });
+
+    e.preventDefault();
+  });
+
+  if ($('#document-list .selected').length == 0) {
+    $('#document-list li a').first().click();
+  }
+
+  $('#save-button').click(function() {
+    var id = $('#document-list .selected').itemID(),
+        params = { d: { data: $('#editor').val(), id: id } };
+    $.put('/documents/' + id + '.json', params, function(data) {
+      // Saved, will return JSON
+    });
+  });
+
+  $(window).resize(resize);
+  $(window).focus(resize);
+  resize();
 })();
